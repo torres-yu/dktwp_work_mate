75dda9349444711ec3cf69040282148a1ac5b3df
torres-bro <torres.bro@kakaocorp.com>
2024-08-27 10:49:33 +0900

    - init

---
+++ src/main/java/io/dkt/dktwp_work_mate/DktwpWorkMateApplication.java
@@ -0,0 +1,13 @@
+package io.dkt.dktwp_work_mate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DktwpWorkMateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DktwpWorkMateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/dkt/dktwp_work_mate/DktwpWorkMateApplicationTests.java
@@ -0,0 +1,13 @@
+package io.dkt.dktwp_work_mate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DktwpWorkMateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
